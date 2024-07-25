72c66127945d8619158e014627490c230ad3e460
JayDDL <dlittle.cdiem@gmail.com>
2024-07-25 12:15:21 +0200

    [Feat] Implemented server.js

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { sequelize } = require('./models'); // Sequelize models
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+const userRoutes = require('./Routes/userRoutes');
+const recipeRoutes = require('./Routes/recipeRoutes');
+const mealPlanRoutes = require('./Routes/mealPlanRoutes');
+
+app.use('/users', userRoutes);
+app.use('/recipes', recipeRoutes);
+app.use('/mealplans', mealPlanRoutes);
+
+// Sync database and start server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
