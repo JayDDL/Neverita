c1e78ed23a5ee7087c6c8c6f68a3e7c745879d18
JayDDL <dlittle.cdiem@gmail.com>
2024-07-25 15:58:09 +0200

    [Feat] Implemented GET & POST recipeRoutes

---
+++ Server/Routes/recipeRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { getAllRecipes, createRecipe } = require('../Controllers/recipeController');
+
+const recipeRouter = express.Router();
+
+// GET requests to '/recipes'
+recipeRouter.get('/recipes', async (req, res) => {
+  try {
+    await getAllRecipes(req, res);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// POST requests to '/recipes'
+recipeRouter.post('/recipes', async (req, res) => {
+  try {
+    await createRecipe(req, res);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = recipeRouter;
