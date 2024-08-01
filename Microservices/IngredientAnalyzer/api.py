29fac4ebf1d3a4965339a9cbf8e0ac717ce6daf0
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-08-01 18:58:40 +0100

    fix(pymicro): cors
    added cors, so the api could communicate with FE

--- Microservices/IngredientAnalyzer/api.py
+++ Microservices/IngredientAnalyzer/api.py
@@ -1,23 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from pyfood.utils import Shelf
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app)
 api = Api(app)
 
 
 def process_diet_request(data):
     shelf = Shelf(region="United Kingdom")
+    print(data)
     try:
         result = shelf.process_ingredients(data["ingredients"])["labels"]
         print(result)
-        if(result['vegan']):
-          result = 'vegan'
-        elif(result['vege']):
-          result = 'vegetarian'
+        if result["vegan"]:
+            result = "vegan"
+        elif result["vege"]:
+            result = "vegetarian"
         else:
-          result = 'carnivore'
+            result = "carnivore"
     except:
         result = "uncertain"
     print(result)
@@ -25,7 +27,7 @@ def process_diet_request(data):
 
 
 class Diet(Resource):
-    def get(self):
+    def post(self):
         data = request.get_json()
         result = process_diet_request(data)
         return jsonify({"status": "success", "result": result})
