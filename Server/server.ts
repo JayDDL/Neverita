45e6f056d3c24696f9942ea3adad42403ba13043
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-02 10:31:02 +0100

    feat(server): implement request logging

--- Server/server.ts
+++ Server/server.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, {
+	type NextFunction,
+	type Request,
+	type Response,
+} from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import sequelize from "./models";
+import { db } from "./models/index";
 
 import { userRouter } from "./Routes/userRoutes";
 import { recipeRouter } from "./Routes/recipeRoutes";
@@ -12,11 +16,29 @@ import { weeklyMealPlanRouter } from "./Routes/weeklyMealPlannerRoutes";
 const app = express(); // Create an instance of the Express application
 const PORT = process.env.PORT || 3000; // Set the port number
 
+const currentTime = new Date()
+	.toLocaleTimeString("en-GB", {
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+		hour12: false,
+		timeZoneName: "short",
+	})
+	.replace(/ GMT.*/, "");
+
 // Middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+	console.log(`${currentTime}: ${req.originalUrl}`);
+	next();
+});
 app.use(bodyParser.json()); // Use body-parser middleware to parse JSON requests
 app.use(cors()); // Use CORS middleware to enable Cross-Origin Resource Sharing
 
 // Routes
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
 app.use("/users", userRouter); // Use user routes for /users endpoint
 app.use("/recipes", recipeRouter); // Use recipe routes for /recipes endpoint
 app.use("/mealplans", mealPlanRouter); // Use meal plan routes for /mealplans endpoint
@@ -24,10 +46,8 @@ app.use("/daily-meal-plans", dailyMealPlanRouter); // Use daily meal plan routes
 app.use("/weekly-meal-plans", weeklyMealPlanRouter); // Use weekly meal plan routes for /weekly-meal-plans endpoint
 
 // Sync database and start server
-sequelize.sync().then(() => {
+db.sequelize.sync().then(() => {
 	app.listen(PORT, () => {
-		console.log(
-			`\n 🚀🚀🚀🚀🚀 Server is running on http://localhost:${PORT} 🚀🚀🚀🚀🚀`,
-		);
+		console.log(`\nServer is running on http://localhost:${PORT}`);
 	});
 });
