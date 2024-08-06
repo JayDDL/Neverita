9b432e5e9f8ec484589525862624e9cd54effaf1
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-08-06 15:14:51 +0100

    edit(mockdata): changed mock data to represent the new data strucuture

--- Client/src/mocks/mock.ts
+++ Client/src/mocks/mock.ts
@@ -5,33 +5,40 @@ export const mockRecipes = [
     id: 1,
     name: 'Test Recipe',
     description: 'A test recipe description',
-    ingredients: [
-      { name: 'Ingredient 1', weight: '100g', preparationType: 'Chopped', cookingMethod: 'Fried' }
+    recipeIngredients: [
+      { ingredients : {name: 'Ingredient 1', id:1, userId:1}, quantity: '100g', preparationType: 'Chopped', cookingMethod: 'Fried' }
     ]
   },
   {
     id: 2,
     name: 'Test Recipe 2',
     description: 'A test recipe description 2',
-    ingredients: [
-      { name: 'Ingredient 1', weight: '100g', preparationType: 'Chopped', cookingMethod: 'Fried' }
+    recipeIngredients: [
+      { ingredients : {name: 'Ingredient 2', id:2, userId:1}, quantity: '100g', preparationType: 'Chopped', cookingMethod: 'Fried' }
     ]
   }
 ]
 
 
 export const mockRecipe = {
-  id: '1',
-  name: 'Test Recipe',
-  description: 'A test recipe description',
-  ingredients: [
-    { name: 'Ingredient 1', weight: '100g', preparationType: 'Chopped', cookingMethod: 'Fried' }
+  id: 1,
+  name: "Test Recipe",
+  description: "Test Description",
+  recipeIngredient: [
+    {
+      Ingredients: {
+        name: "Ingredient 1"
+      },
+      quantity: "100g",
+      preparationType: "Chopped",
+      cookingMethod: "Fried"
+    }
   ]
 };
 
-
-
-export const mockDiet = { result: 'Vegetarian' };
+export const mockDiet = {
+  result: "Vegetarian"
+};
 
 
 
