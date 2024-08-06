824ada3b0610335a0b94215d2c673ce907d64579
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-06 14:25:06 +0100

    chore(server): remove comments/clean up in server.ts

--- Server/server.ts
+++ Server/server.ts
@@ -25,19 +25,18 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 	console.log(`${currentTime}: ${req.originalUrl}`);
 	next();
 });
-app.use(bodyParser.json()); // Use body-parser middleware to parse JSON requests
-app.use(cors()); // Use CORS middleware to enable Cross-Origin Resource Sharing
+app.use(bodyParser.json());
+app.use(cors());
 
 // ROUTES
 app.get("/", (req: Request, res: Response) => {
 	res.status(200).send("Neverita API is running");
 });
+app.use("/users", userRouter);
+app.use("/user/:userId/recipes", recipeRouter);
+app.use("/user/:userId/mealplans", mealPlanRouter);
 
-app.use("/users", userRouter); // Use user routes for /users endpoint
-app.use("/user/:userId/recipes", recipeRouter); // Use recipe routes for /recipes endpoint
-app.use("/user/:userId/mealplans", mealPlanRouter); // Use daily meal plan routes for /daily-meal-plans endpoint
-
-// Sync database and start server
+// START SERVER
 app.listen(PORT, () => {
 	console.log(`Server listening on http://localhost:${PORT}`);
 });
