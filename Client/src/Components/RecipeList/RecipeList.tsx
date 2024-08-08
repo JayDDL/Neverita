d6343f50c6663daab07ed3c8aebc33820d1d4562
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-08 12:43:39 +0100

    fix(css): remove dark mode properties

--- Client/src/Components/RecipeList/RecipeList.tsx
+++ Client/src/Components/RecipeList/RecipeList.tsx
@@ -1,30 +1,30 @@
-import { RecipeProps } from "../../types";
+import type { RecipeProps } from "../../types";
 
 // Define the RecipeList functional component, which takes recipes and onViewClick as props
 export const RecipeList = ({ recipes, onViewClick }: RecipeProps) => {
-  return (
-    <div className="flex flex-wrap gap-4">
-      {recipes.map((recipe) => (
-        <div
-          key={recipe.id}
-          className="flex w-full items-center overflow-hidden rounded-lg border border-gray-400 bg-white p-4"
-        >
-          <div className="flex-grow">
-            <div className="recipe-title mb-2 text-lg font-medium">
-              {recipe.name}
-            </div>
-            <div className="recipe-description text-gray-600">
-              {recipe.description}
-            </div>
-          </div>
-          <button
-            className="view-button ml-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
-            onClick={() => onViewClick(recipe.id)}
-          >
-            View
-          </button>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="flex flex-wrap gap-4">
+			{recipes.map((recipe) => (
+				<div
+					key={recipe.id}
+					className="flex min-w-full items-center overflow-hidden rounded-lg border border-gray-400 bg-white p-4"
+				>
+					<div className="flex-grow">
+						<div className="recipe-title mb-2 text-lg font-medium">
+							{recipe.name}
+						</div>
+						<div className="recipe-description text-gray-600">
+							{recipe.description}
+						</div>
+					</div>
+					<button
+						className="view-button ml-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
+						onClick={() => onViewClick(recipe.id)}
+					>
+						View
+					</button>
+				</div>
+			))}
+		</div>
+	);
 };
