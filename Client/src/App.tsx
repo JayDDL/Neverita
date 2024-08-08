d26f8d1db0e0a00aa79a7c83a626ed8c5647807f
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-08 12:38:30 +0100

    feat(routing): implement profile page routing

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -6,48 +6,51 @@ import { DailyMealPlanner } from "./Components/DailyMealPlanner/DailyMealPlanner
 import { useState } from "react";
 import { Login } from "./Components/Login/Login";
 import { Navbar } from "./Components/NavBar/NavBar";
+import Profile from "./Components/Profile/Profile";
 
 const App = () => {
-  const [userId, setUserId] = useState<null | number>(null);  
+	const [userId, setUserId] = useState<null | number>(null);
 
-  const loggedIn = (
-    userId &&
-    <Router>
-      <div className="flex flex-wrap-reverse justify-center h-screen ">
-        <footer className="h-16">
-    <Navbar/>
-    </footer>
-    <div className="w-full p-3">
-        <Routes>
-          <Route
-            path="/view-recipes"
-            element={<ViewRecipes userId={userId} />}
-          />
-          <Route
-            path="/recipe/:recipeId"
-            element={<RecipeDetails userId={userId} />}
-          />
-          <Route
-            path="/create-recipe"
-            element={<CreateRecipe userId={userId} />}
-          />
-          <Route
-            path="/daily-meal-planner"
-            element={<DailyMealPlanner userId={userId} />}
-          />
-        </Routes>
-        </div>
-        </div>
-    </Router>
-  );
+	const loggedIn = userId && (
+		<Router>
+			<div className="flex flex-wrap-reverse justify-center h-screen ">
+				<footer className="w-full h-16">
+					<Navbar />
+				</footer>
+				<div className="w-full p-3">
+					<Routes>
+						<Route
+							path="/view-recipes"
+							element={<ViewRecipes userId={userId} />}
+						/>
+						<Route
+							path="/recipe/:recipeId"
+							element={<RecipeDetails userId={userId} />}
+						/>
+						<Route
+							path="/create-recipe"
+							element={<CreateRecipe userId={userId} />}
+						/>
+						<Route
+							path="/daily-meal-planner"
+							element={<DailyMealPlanner userId={userId} />}
+						/>
+						<Route path="/profile" element={<Profile />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 
-  const loggedOut = (<>
-  <Router>
-  <Login setUserId = {setUserId}/> 
-  </Router>
-  </>);
+	const loggedOut = (
+		<>
+			<Router>
+				<Login setUserId={setUserId} />
+			</Router>
+		</>
+	);
 
-  return <>{userId ? loggedIn : loggedOut}</>;
+	return <>{userId ? loggedIn : loggedOut}</>;
 };
 
 export default App;
